dcd0d242bfe36ceecfef9e2a6a09bbe35d1fcfb1
Tatsuya Nakajima <aobatamago@amail.plala.or.jp>
2023-06-02 15:07:19 +0900

    Add method to delete all records

--- Import-CliKintone.bat
+++ Import-CliKintone.bat
@@ -22,21 +22,24 @@ REM echo datetimestr = %datetimestr%
 REM make a relative file path for logs.
 set IMPORTLOGFILE=.\logs\Cli-Kintone-BAT_%datetimestr%.log
 
+REM ####################↓↓↓ IMPORT ↓↓↓####################
 REM Execute a PowerShell script.
 REM ALL
-REM PowerShell -NoProfile -ExecutionPolicy Unrestricted -File Import-Clikintone.ps1 1> %IMPORTLOGFILE% 2>&1 3>&1 4>&1 5>&1 6>&1
-
+REM PowerShell -NoProfile -ExecutionPolicy Unrestricted -File Import-Clikintone.ps1 -batmode import 1> %IMPORTLOGFILE% 2>&1 3>&1 4>&1 5>&1 6>&1
 REM APPLICATION 1
-PowerShell -NoProfile -ExecutionPolicy Unrestricted -File Import-Clikintone.ps1 -application "cli-kintone import csv data" 1> %IMPORTLOGFILE% 2>&1 3>&1 4>&1 5>&1 6>&1
-
+REM PowerShell -NoProfile -ExecutionPolicy Unrestricted -File Import-Clikintone.ps1 -batmode import -application "import csv with attachments files" 1> %IMPORTLOGFILE% 2>&1 3>&1 4>&1 5>&1 6>&1
 REM APPLICATION 2
-REM PowerShell -NoProfile -ExecutionPolicy Unrestricted -File Import-Clikintone.ps1 -application "cli-kintone import csv data" 1> %IMPORTLOGFILE% 2>&1 3>&1 4>&1 5>&1 6>&1
-
+PowerShell -NoProfile -ExecutionPolicy Unrestricted -File Import-Clikintone.ps1 -batmode import -application "cli-kintone import csv data" 1> %IMPORTLOGFILE% 2>&1 3>&1 4>&1 5>&1 6>&1
 REM APPLICATION 3
-REM PowerShell -NoProfile -ExecutionPolicy Unrestricted -File Import-Clikintone.ps1 -application "Dummy1" 1> %IMPORTLOGFILE% 2>&1 3>&1 4>&1 5>&1 6>&1
-
+REM PowerShell -NoProfile -ExecutionPolicy Unrestricted -File Import-Clikintone.ps1 -batmode import -application "Dummy1" 1> %IMPORTLOGFILE% 2>&1 3>&1 4>&1 5>&1 6>&1
 REM APPLICATION 4
-REM PowerShell -NoProfile -ExecutionPolicy Unrestricted -File Import-Clikintone.ps1 -application "Dummy2" 1> %IMPORTLOGFILE% 2>&1 3>&1 4>&1 5>&1 6>&1
+REM PowerShell -NoProfile -ExecutionPolicy Unrestricted -File Import-Clikintone.ps1 -batmode import -application "Dummy2" 1> %IMPORTLOGFILE% 2>&1 3>&1 4>&1 5>&1 6>&1
+REM ####################↑↑↑ IMPORT ↑↑↑####################
+
+
+REM #################↓↓↓ DELETE ALL RECORDS ↓↓↓############################
+REM PowerShell -NoProfile -ExecutionPolicy Unrestricted -File Import-Clikintone.ps1 -batmode delete -application "cli-kintone import csv data" 1> %IMPORTLOGFILE% 2>&1 3>&1 4>&1 5>&1 6>&1
+REM #################↑↑↑ DELETE ALL RECORDS ↑↑↑############################
 
 pause
 if not %ERRORLEVEL%==0 (Exit /b 1) else (Exit /b 0)
--- Import-Clikintone.ps1
+++ Import-Clikintone.ps1
@@ -9,7 +9,8 @@ NOTE: Use the cli-kintone utility to import CSV file data and attachments.
 #>
 [cmdletbinding()]
 param (
-    [string]$application = ""
+    [Parameter(Mandatory=$true)][ValidateSet("import", "delete")][string]$batmode = "import",
+    [Parameter(Mandatory=$true)][string]$application = ""
 )
 
 # 関数のエラーはキャッチできるようにする。
@@ -49,13 +50,14 @@ function Get-CurrentDirectoryPath {
 function Invoke-Clikintone {
     [CmdletBinding()]
     param(
+        [Parameter(Mandatory=$true)][ValidateSet("import", "delete")][string]$mode,
         [Parameter(Mandatory=$true)][string]$logpath,
         [Parameter(Mandatory=$true)][string]$logname,
         [Parameter(Mandatory=$true)][string]$baseUrl,
         [Parameter(Mandatory=$true)][string]$appToken,
         [Parameter(Mandatory=$true)][string]$appId,
-        [Parameter(Mandatory=$true)][string]$csvFilePath,
-        [Parameter(Mandatory=$false)][ValidateSet("utf8", "sjis")]$charEncoding,
+        [Parameter(Mandatory=$false)][string]$csvFilePath,
+        [Parameter(Mandatory=$false)][ValidateSet("utf8", "sjis")][string]$charEncoding,
         [Parameter(Mandatory=$false)][string]$updateKey = "",
         [Parameter(Mandatory=$false)][string]$attachments = ""
     )
@@ -65,45 +67,54 @@ function Invoke-Clikintone {
     }
 
     process {
-        if (($updateKey -eq "") -and ($attachments -eq "")) {
-            Log -LogPath $logpath -LogName $logname -LogString "not upsert and not attachments"
-            .\cli-kintone.exe record import `
+        if ($mode -eq "delete") {
+            Log -LogPath $logpath -LogName $logname -LogString "DELETE ALL RECORD!"
+            .\cli-kintone.exe record delete `
                 --base-url $baseUrl `
                 --api-token $appToken `
-                --app $appId `
-                --file-path $csvFilePath `
-                --encoding $charEncoding
-        }
-        elseif (($updateKey -ne "") -and ($attachments -eq "")) {
-            Log -LogPath $logpath -LogName $logname -LogString "upsert and not attachments"
-            .\cli-kintone.exe record import `
-                --base-url $baseUrl `
-                --api-token $appToken `
-                --app $appId `
-                --file-path $csvFilePath `
-                --encoding $charEncoding `
-                --update-key $updateKey
-        }
-        elseif (($updateKey -eq "") -and ($attachments -ne "")) {
-            Log -LogPath $logpath -LogName $logname -LogString "not upsert and attachments"
-            .\cli-kintone.exe record import `
-                --base-url $baseUrl `
-                --api-token $appToken `
-                --app $appId `
-                --file-path $csvFilePath `
-                --encoding $charEncoding `
-                --attachments-dir $attachments
+                --app $appId
         }
         else {
-            Log -LogPath $logpath -LogName $logname -LogString "upsert and attachments"
-            .\cli-kintone.exe record import `
-                --base-url $baseUrl `
-                --api-token $appToken `
-                --app $appId `
-                --file-path $csvFilePath `
-                --encoding $charEncoding `
-                --update-key $updateKey `
-                --attachments-dir $attachments
+            if (($updateKey -eq "") -and ($attachments -eq "")) {
+                Log -LogPath $logpath -LogName $logname -LogString "not upsert and not attachments"
+                .\cli-kintone.exe record import `
+                    --base-url $baseUrl `
+                    --api-token $appToken `
+                    --app $appId `
+                    --file-path $csvFilePath `
+                    --encoding $charEncoding
+            }
+            elseif (($updateKey -ne "") -and ($attachments -eq "")) {
+                Log -LogPath $logpath -LogName $logname -LogString "upsert and not attachments"
+                .\cli-kintone.exe record import `
+                    --base-url $baseUrl `
+                    --api-token $appToken `
+                    --app $appId `
+                    --file-path $csvFilePath `
+                    --encoding $charEncoding `
+                    --update-key $updateKey
+            }
+            elseif (($updateKey -eq "") -and ($attachments -ne "")) {
+                Log -LogPath $logpath -LogName $logname -LogString "not upsert and attachments"
+                .\cli-kintone.exe record import `
+                    --base-url $baseUrl `
+                    --api-token $appToken `
+                    --app $appId `
+                    --file-path $csvFilePath `
+                    --encoding $charEncoding `
+                    --attachments-dir $attachments
+            }
+            else {
+                Log -LogPath $logpath -LogName $logname -LogString "upsert and attachments"
+                .\cli-kintone.exe record import `
+                    --base-url $baseUrl `
+                    --api-token $appToken `
+                    --app $appId `
+                    --file-path $csvFilePath `
+                    --encoding $charEncoding `
+                    --update-key $updateKey `
+                    --attachments-dir $attachments
+            }
         }
     }
 }
@@ -122,42 +133,70 @@ $retString = PurgeFiles -PurgePath $setting.batsettings.logfilepath -PastDays $s
 Log -LogPath $setting.batsettings.logfilepath -LogName $setting.batsettings.logfilename -LogString ("不要ログ削除：{0}" -f $retString)
 
 # 処理開始
-Log -LogPath $setting.batsettings.logfilepath `
+if ($batmode -eq "delete") {
+    Log -LogPath $setting.batsettings.logfilepath `
+    -LogName $setting.batsettings.logfilename `
+    -LogString "cli-Kintoneを使用して対象アプリのレコードをすべて削除します。（CAN NOT ROLLBACK）。"
+
+    foreach ($item in $setting.apps) {
+        if ("" -eq $application) {
+            Log -LogPath $setting.batsettings.logfilepath `
+                -LogName $setting.batsettings.logfilename `
+                -LogString ("アプリケーション:{0}の全レコードを削除します。" -f $item.appname)
+            $timeSpan = Measure-Command {
+                Invoke-Clikintone `
+                    -mode delete `
+                    -logpath $setting.batsettings.logfilepath `
+                    -logname $setting.batsettings.logfilename `
+                    -baseUrl $setting.baseurl `
+                    -appToken $item.apptoken `
+                    -appId $item.appid
+            }
+            Log -LogPath $setting.batsettings.logfilepath `
+                -LogName $setting.batsettings.logfilename `
+                -LogString ("処理時間：{0}" -f $timeSpan.ToString("d'日'h'時間'm'分's'秒'fff"))
+            Log -LogPath $setting.batsettings.logfilepath `
+                -LogName $setting.batsettings.logfilename `
+                -LogString ("アプリケーション:{0}の全レコードを削除しました。" -f $item.appname)
+        }
+        else {
+            if ($application -eq $item.appname) {
+                Log -LogPath $setting.batsettings.logfilepath `
+                    -LogName $setting.batsettings.logfilename `
+                    -LogString ("アプリケーション:{0}の全レコードを削除します。" -f $item.appname)
+                $timeSpan = Measure-Command {
+                    Invoke-Clikintone `
+                        -mode delete `
+                        -logpath $setting.batsettings.logfilepath `
+                        -logname $setting.batsettings.logfilename `
+                        -baseUrl $setting.baseurl `
+                        -appToken $item.apptoken `
+                        -appId $item.appid
+                }
+                Log -LogPath $setting.batsettings.logfilepath `
+                    -LogName $setting.batsettings.logfilename `
+                    -LogString ("処理時間：{0}" -f $timeSpan.ToString("d'日'h'時間'm'分's'秒'fff"))
+                Log -LogPath $setting.batsettings.logfilepath `
+                    -LogName $setting.batsettings.logfilename `
+                    -LogString ("アプリケーション:{0}の全レコードを削除しました。" -f $item.appname)
+            }
+        }
+    }
+}
+else {
+    Log -LogPath $setting.batsettings.logfilepath `
     -LogName $setting.batsettings.logfilename `
     -LogString "cli-Kintoneを使用してCSVデータのインポートを開始します。"
 
-foreach ($item in $setting.apps) {
-    if ("" -eq $application) {
-        Log -LogPath $setting.batsettings.logfilepath `
-            -LogName $setting.batsettings.logfilename `
-            -LogString ("アプリケーション:{0}に、CSVをインポートします。" -f $item.appname)
-        
-        $timeSpan = Measure-Command {
-            Invoke-Clikintone `
-                -logpath $setting.batsettings.logfilepath `
-                -logname $setting.batsettings.logfilename `
-                -baseUrl $setting.baseurl `
-                -appToken $item.apptoken `
-                -appId $item.appid `
-                -csvFilePath $item.targetcsv `
-                -charEncoding $item.csvencoding `
-                -updateKey $item.updatekey `
-                -attachments $item.attachments
-        }
-        Log -LogPath $setting.batsettings.logfilepath `
-            -LogName $setting.batsettings.logfilename `
-            -LogString ("処理時間：{0}" -f $timeSpan.ToString("d'日'h'時間'm'分's'秒'fff"))
-        Log -LogPath $setting.batsettings.logfilepath `
-            -LogName $setting.batsettings.logfilename `
-            -LogString ("アプリケーション:{0}へ、CSVをインポートしました。" -f $item.appname)
-    } else {
-        if ($application -eq $item.appname) {
+    foreach ($item in $setting.apps) {
+        if ("" -eq $application) {
             Log -LogPath $setting.batsettings.logfilepath `
                 -LogName $setting.batsettings.logfilename `
                 -LogString ("アプリケーション:{0}に、CSVをインポートします。" -f $item.appname)
-        
+            
             $timeSpan = Measure-Command {
                 Invoke-Clikintone `
+                    -mode import `
                     -logpath $setting.batsettings.logfilepath `
                     -logname $setting.batsettings.logfilename `
                     -baseUrl $setting.baseurl `
@@ -168,18 +207,45 @@ foreach ($item in $setting.apps) {
                     -updateKey $item.updatekey `
                     -attachments $item.attachments
             }
-            
             Log -LogPath $setting.batsettings.logfilepath `
                 -LogName $setting.batsettings.logfilename `
                 -LogString ("処理時間：{0}" -f $timeSpan.ToString("d'日'h'時間'm'分's'秒'fff"))
             Log -LogPath $setting.batsettings.logfilepath `
                 -LogName $setting.batsettings.logfilename `
                 -LogString ("アプリケーション:{0}へ、CSVをインポートしました。" -f $item.appname)
+        } else {
+            if ($application -eq $item.appname) {
+                Log -LogPath $setting.batsettings.logfilepath `
+                    -LogName $setting.batsettings.logfilename `
+                    -LogString ("アプリケーション:{0}に、CSVをインポートします。" -f $item.appname)
+            
+                $timeSpan = Measure-Command {
+                    Invoke-Clikintone `
+                        -mode import `
+                        -logpath $setting.batsettings.logfilepath `
+                        -logname $setting.batsettings.logfilename `
+                        -baseUrl $setting.baseurl `
+                        -appToken $item.apptoken `
+                        -appId $item.appid `
+                        -csvFilePath $item.targetcsv `
+                        -charEncoding $item.csvencoding `
+                        -updateKey $item.updatekey `
+                        -attachments $item.attachments
+                }
+                
+                Log -LogPath $setting.batsettings.logfilepath `
+                    -LogName $setting.batsettings.logfilename `
+                    -LogString ("処理時間：{0}" -f $timeSpan.ToString("d'日'h'時間'm'分's'秒'fff"))
+                Log -LogPath $setting.batsettings.logfilepath `
+                    -LogName $setting.batsettings.logfilename `
+                    -LogString ("アプリケーション:{0}へ、CSVをインポートしました。" -f $item.appname)
+            }
         }
     }
+
+    # 処理終了
+    Log -LogPath $setting.batsettings.logfilepath `
+        -LogName $setting.batsettings.logfilename `
+        -LogString "cli-Kintoneを使用してCSVデータのインポートが完了しました。"
 }
 
-# 処理終了
-Log -LogPath $setting.batsettings.logfilepath `
-    -LogName $setting.batsettings.logfilename `
-    -LogString "cli-Kintoneを使用してCSVデータのインポートが完了しました。"
